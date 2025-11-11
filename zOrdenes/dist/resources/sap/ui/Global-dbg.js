253f5ebe6fe0f2cb25cde25a5067d289ffaf89ca
Abel <abel.cruz@negociando.com.mx>
2025-11-11 17:11:46 -0600

    1er commit

--- zOrdenes/dist/resources/sap/ui/Global-dbg.js
+++ zOrdenes/dist/resources/sap/ui/Global-dbg.js
@@ -61,7 +61,7 @@ sap.ui.define([
 		 */
 		version: "1.136.1",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20251111-1706" }
+		buildinfo : { lastchange : "", buildtime : "20251111-1709" }
 	};
 
 	/**
